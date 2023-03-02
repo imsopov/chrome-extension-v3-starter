441151cf9788eeef28afb0088a5fb86932fb7927
Scott Taylor <scott.taylor@envido.com.au>
2023-03-02 16:23:28 +1030

    Photo Finish Tools

--- foreground.js
+++ foreground.js
@@ -1,7 +1,62 @@
-// This script gets injected into any opened page
-// whose URL matches the pattern defined in the manifest
-// (see "content_script" key).
-// Several foreground scripts can be declared
-// and injected into the same or different pages.
 
-console.log("This prints to the console of the page (injected only if the page url matched)")
+const callback = function (mutationList, observer) {
+
+    const newRaces = [];
+    var races = document.querySelector("ul[data-cy='races-sub-races-open-for-entry-container']");
+
+    mutationList.forEach(function (mutation) {
+        mutation.addedNodes.forEach(function (item) {
+            var derbyvalues = item.querySelectorAll(".space-x-1");
+            convertToUSD(derbyvalues);
+
+        });
+        observer.disconnect();
+        var config = { childList: true };
+        observer.observe(races, config);
+    });
+   
+};
+
+
+function convertToUSD(listOfRaces) {
+
+    listOfRaces.forEach(function (race) {
+
+        const text = race.textContent;
+        const match = text.match(/\d+\.\d/);
+        if (match) {
+            let usd = 0;
+            if (match.input.includes('K')) {
+                usd = ((match.toString() * 1000) / 80).toFixed(2)
+            }
+            else {
+                usd = (match.toString() / 80).toFixed(2);
+            }
+            race.insertAdjacentHTML('afterend', '<p class="font-inter text-base text-white"> $' + usd + '</p>');
+        }
+    });
+}
+
+function addObserverIfDesiredNodeAvailable() {
+
+    var races = document.querySelector("ul[data-cy='races-sub-races-open-for-entry-container']")
+    if (!races) {
+        //The node we need does not exist yet.
+        //Wait 500ms and try again
+        window.setTimeout(addObserverIfDesiredNodeAvailable, 500);
+        return;
+    }
+
+    convertToUSD(document.querySelectorAll(".space-x-1"));
+    var config = { childList: true };
+    const observer = new MutationObserver(callback);
+    observer.observe(races, config);
+
+}
+
+addObserverIfDesiredNodeAvailable();
+
+
+
+
+
